d3859d31702a01f860a954f53ea8faa8bb347e28
akm <akm2000@gmail.com>
2017-11-09 23:51:38 +0900

    :+1: Add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Post.create!(title: "test1", body: "foo\nbar\nbaz", rating: 4).tap do |p1|
+  p1.comments.create!(body: "Good!")
+  p1.comments.create!(body: "Nice!!")
+end
+
+Post.create!(title: "test2", body: "dangerous!", rating: 12).tap do |p2|
+  p2.comments.create!(body: "are you sure?")
+  p2.comments.create!(body: "crazy!")
+end
