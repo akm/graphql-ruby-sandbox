73062d879df711c2ada126bbc2d07fada45138b1
akm <akm2000@gmail.com>
2017-11-09 23:40:53 +0900

    Declare types
    http://graphql-ruby.org/getting_started#declare-types

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,6 +1,9 @@
 Types::PostType = GraphQL::ObjectType.define do
   name "Post"
-  field :title, types.String
-  field :rating, types.Int
-  field :comments, types[Types::CommentType]
+  description "A blog post"
+  # `!` marks a field as "non-null"
+  field :id, !types.ID
+  field :title, !types.String
+  field :body, !types.String
+  field :comments, types[!Types::CommentType]
 end
